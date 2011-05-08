f9ea19c72f029595c86b8791ea0354c676d36fbf
Sean Powers <spsimplicity@gmail.com>
2011-05-08 17:36:16 -0500

    MySQL Database creation script. Run with "source /path/to/crawler_db.sql"

---
+++ crawler_db.sql
@@ -0,0 +1,62 @@
+create database if not exists CRAWLER;
+use CRAWLER;
+
+create table if not exists DEPARTMENT
+(
+    ID int auto_increment not null primary key,
+	NAME varchar(50) not null
+) type=InnoDB;
+
+create table if not exists INSTRUCTOR
+(
+    ID int auto_increment not null primary key,
+	SEED varchar(255) not null,
+	SOCIAL int,
+	POLYRATING varchar(255) not null,
+	NAME varchar(50) not null,
+	USERNAME varchar(50),
+	DEPARTMENT int not null,
+	foreign key (DEPARTMENT) references DEPARTMENT(ID) on delete cascade
+) type=InnoDB;
+
+create table if not exists PUBLICATION
+(
+    ID int auto_increment not null primary key,
+	TITLE varchar(200) not null,
+	LINK varchar(255) not null
+) type=InnoDB;
+
+create table if not exists OFFICE
+(
+    ID int auto_increment not null primary key,
+	BUILDING int not null,
+	ROOM int not null
+) type=InnoDB;
+
+create table if not exists CLASS
+(
+    ID int auto_increment not null primary key,
+	DESCRIPTION longtext,
+	NAME varchar(150) not null,
+	CLASSNUMBER int not null,
+	INSTRUCTOR int not null,
+	foreign key (INSTRUCTOR) references INSTRUCTOR(ID) on delete cascade
+) type=InnoDB;
+
+create table if not exists WROTE
+(
+    ID int auto_increment not null primary key,
+	INSTRUCTOR int not null,
+	PUBLICATION int not null,
+	foreign key (INSTRUCTOR) references INSTRUCTOR(ID) on delete cascade,
+	foreign key (PUBLICATION) references PUBLICATION(ID) on delete cascade
+) type=InnoDB;
+
+create table if not exists LOCATED
+(
+    ID int auto_increment not null primary key,
+	INSTRUCTOR int not null,
+	OFFICE int not null,
+	foreign key (INSTRUCTOR) references INSTRUCTOR(ID) on delete cascade,
+	foreign key (OFFICE) references OFFICE(ID) on delete cascade
+) type=InnoDB;
